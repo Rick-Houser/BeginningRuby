ce8c6517fe7cfe29ead1af79b05dee7a9fa7e4da
FrackenToaster <frakentoaster@gmail.com>
2015-07-19 12:18:02 -0700

    Reorganize files and folders

---
+++ file_analyzer/README.md
@@ -0,0 +1,21 @@
+# Text Analyzer
+
+
+This small program was created from chapter 4 of Peter Cooper's book Beginning Ruby.
+The analyzer app will take in a text file, calculate and then output the following:
+
+- Number of lines
+- Number of characters
+- Number of characters (excluding spaces)
+- Number of words
+- Number of sentences
+- Number of paragraphs
+- Number of sentences per paragraph (average)
+- Number of words per sentence (average)
+- Number of words (excluding stop words)
+- Gives a short summary of the text excluding stop words.
+
+To run this app, be sure to supply the text file name to be analyzed.
+
+
+For more information on stop words, visit http://en.wikipedia.org/wiki/Stop_words
---
+++ file_analyzer/analyzer.rb
@@ -0,0 +1,45 @@
+# "You initialize text variable and line_count, iterate over it and increment it by 1 each time.
+# Next you push each line into "text" variable to be used again.
+
+stopwords = %w{the a by on for of are with just but and to the my I has some in}
+lines = File.readlines(ARGV[0])
+line_count = lines.size
+text = lines.join
+
+# Count the characters
+total_characters = text.length
+total_characters_nospaces = text.gsub(/\s+/, '').length
+
+# Count the words, sentences and paragraphs
+word_count = text.split.length
+sentence_count = text.split(/\.|\?|!/).length
+paragraph_count = text.split(/\n\n/).length
+
+# Make a list of words in the text that aren't stop words,
+# count them, and work out the percentage of non-stop words
+# against all words
+words = text.scan(/\w+/)
+keywords = words.select { |word| !stopwords.include?(word) }
+keyword_percentage = ((keywords.length.to_f / words.length.to_f) * 100).to_i
+
+# Summarizethe text by cherry picking some choice sentences
+sentences = text.gsub(/\s+/, ' ').strip.split(/\.|\?|!/)
+sentences_sorted = sentences.sort_by { |sentence| sentence.length }
+one_third = sentences_sorted.length / 3
+ideal_sentences = sentences_sorted.slice(one_third, one_third + 1)
+ideal_sentences = ideal_sentences.select { |sentence| sentence =~ /is|are/ }
+
+# Give the analysis back to the user
+puts "#{line_count} lines"
+puts "#{total_characters} characters"
+puts "#{total_characters_nospaces} characters (excluding spaces)"
+puts "#{word_count} words"
+puts "#{sentence_count} sentences"
+puts "#{paragraph_count} paragraphs"
+puts "#{sentence_count / paragraph_count} sentences per paragraphs (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
+puts "#{keyword_percentage}% of words are non-fluff"
+puts "Summary:\n\n" + ideal_sentences.join(". ")
+puts "-- End of analysis"
+
+
